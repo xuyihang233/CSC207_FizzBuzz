b4f07b063c970e596c9f137bfa4083f1454e6a3a
Yihang Xu <yihangxu2416@gmail.com>
2025-09-08 21:15:54 -0400

    While loop modified with helper method;
    Tasks 1-3 completed...
    
    Working on Task 4

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -6,6 +6,17 @@ public class Multiples {
         System.out.println(mtps);
     }
 
+    public static int multiples() {
+        int count = 0;
+        for (int i = 1; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
     public static int multiples(int n, int a, int b) {
         int count = 0;
         for (int i = 1; i < n; i++) {
